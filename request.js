3fdc12a5e775059d1078088cc922e812d55bc9f9
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-07-11 21:24:07 +0200

    V0.0.2 - Cleaned up code

--- request.js
+++ request.js
@@ -1,13 +1,10 @@
 
-//
 var url = require('url');
 var http = require('http');
 var https = require('https');
 
-//
 var extend = require('extend');
 
-//
 var defaultRequestOptions = {
     'method': 'GET',
     'enableSSL': false,
@@ -16,43 +13,35 @@ var defaultRequestOptions = {
     }
 };
 
-//
 exports = module.exports = function(options, callback) {
 
-    //
-    console.log('started');
     var start;
 
+    if(typeof options === 'string') options = url.parse(options);
     options = extend(true, defaultRequestOptions, options);
 
-    //
     var protocol = (options.enableSSL) ? https : http;
     var request = protocol.request(options, function(response) {
-        //
         response.setEncoding('utf8');
         response.body = '';
         response.on('data', function(data) {
             response.body += data;
         });
 
-        //
         response.on('end', function() {
             response.elapsed = process.hrtime(start)[1] / 1000000; // in milliseconds
             return callback(null, response);
         });
     });
 
-    //
     request.on('socket', function (response) {
         start = process.hrtime();
     });
 
-    //
     request.on('error', function (error) {
         return callback(error);
     });
 
-    //
     request.end();
 
 };
