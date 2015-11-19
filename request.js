5afadc3365a59e68216de61ca5f1cb305a9e10e9
Michiel van der Velde <michiel@michielvdvelde.nl>
2015-11-19 09:30:31 +0100

    Improve protocol selection

--- request.js
+++ request.js
@@ -43,8 +43,8 @@ exports = module.exports = function(options, setOptions, callback) {
     var elapsed = 0;
     if(typeof options === 'string') options = url.parse(options);
     options = extend(true, defaultOptions, options);
-    if(!options.port) options.port = (options.protocol == 'https:') ? 443 : 80;
-    var protocol = (options.protocol == 'https:') ? https : http;
+    if(!options.port) options.port = (options.protocol.indexOf('https') != -1) ? 443 : 80;
+    var protocol = (options.protocol.indexOf('https') != -1) ? https : http;
 
     var request = protocol.request(options, function(response) {
         response.elapsed = process.hrtime(elapsed)[1] / 1000000;
